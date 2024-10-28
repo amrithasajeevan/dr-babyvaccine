6914589daa7a341cef65a21772007b4cc3b34164
amrithasajeevan <106527708+amrithasajeevan@users.noreply.github.com>
2024-10-28 17:31:51 +0530

    Update settings.py

--- babyvaccinepro/settings.py
+++ babyvaccinepro/settings.py
@@ -145,5 +145,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'amrithababy142@gmail.com'
-EMAIL_HOST_PASSWORD = 'gngt jofv fidy zmzm'
+EMAIL_HOST_PASSWORD = ''
 
